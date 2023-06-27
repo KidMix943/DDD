cb3c4e8983a3b7835f27a1a366d207729d8e5bea
KidMix943 <137384390+KidMix943@users.noreply.github.com>
2023-06-27 11:08:18 +0300

    Add files via upload

---
+++ Lesson2/Messenger/Messenger.Infrastructure/Context/UserContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Messenger.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messenger.Infrastructure.Context
+{
+    public class UserContext:DbContext
+    {
+        public DbSet<User> Users => Set<User>();
+
+        public UserContext(DbContextOptions options) : base(options)
+        {
+            Database.Migrate();
+        }
+    }
+}
