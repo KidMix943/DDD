e9b339185773449cdba800b38c595deab3c46c93
KidMix943 <137384390+KidMix943@users.noreply.github.com>
2023-06-27 12:25:28 +0300

    Add files via upload

---
+++ Lesson2/Messenger/Messenger.Domain/IUserManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messenger.Domain
+{
+    public interface IUserManager
+    {
+        Task<List<User>> GetAll();
+        Task<User?> Info(long? id);
+        Task<User?> GetById(long id);
+        Task<User> Create(User user);
+        Task<User?> Update(long id, User user);
+        Task<User?> Delete(long id);
+
+    }
+}
